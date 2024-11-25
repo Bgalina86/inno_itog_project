ab905d23ec9c6317b3ab5fba2767e5b32e00f40a
Смирнова Галина <bgalina86@gmail.com>
2024-11-25 16:26:57 +0300

    UI and API, and BD

---
+++ src/test/java/api_inno_itog_project/EmployeeBusinessTests.java
@@ -0,0 +1,275 @@
+package api_inno_itog_project;
+
+import static api_inno_itog_project.x_clients.helper.EmployeeRandomeService.generateEmployee;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.github.javafaker.Faker;
+import api_inno_itog_project.x_clients.ext.DatabaseService;
+import api_inno_itog_project.x_clients.ext.DbProperties;
+import api_inno_itog_project.x_clients.helper.CompanyApiHelper;
+import api_inno_itog_project.x_clients.helper.ConfProperties;
+import api_inno_itog_project.x_clients.helper.EmployeeApiHelper;
+import api_inno_itog_project.x_clients.model.CreateEmployeeResponse;
+import api_inno_itog_project.x_clients.model.Employee;
+import api_inno_itog_project.x_clients.model.PatchEmployeeRequest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import jdk.jfr.Description;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Epic("")
+public class EmploeeBusinessTests {
+
+    private static DatabaseService databaseService;
+    private static int companyId;
+    private static int employeeId;
+    private static CompanyApiHelper companyApiHelper;
+    private static EmployeeApiHelper employeeApiHelper;
+    private static ConfProperties properties;
+    private static String username;
+    private static String password;
+
+    @BeforeAll
+    public static void setUp() throws SQLException, IOException {
+
+        RestAssured.baseURI = DbProperties.getProperties("baseURI");
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        databaseService = new DatabaseService();
+        databaseService.connectToDb();
+        companyId = databaseService.createNewCompany();
+        employeeId = databaseService.createNewEmployee(companyId);
+        databaseService.createNewEmployee(companyId);
+
+        companyApiHelper = new CompanyApiHelper();
+        employeeApiHelper = new EmployeeApiHelper();
+
+        RestAssured.defaultParser = Parser.JSON;
+       properties = new ConfProperties();
+        username = properties.getProperty("username");
+        password = properties.getProperty("password");
+    }
+
+
+    @AfterAll
+    public static void tearDown() throws SQLException {
+        databaseService.deleteCompanyAndItsEmloyees(companyId);
+        databaseService.closeConnection();
+    }
+
+
+    @Test
+    @Description("Проверяем, что могу создать нового пользователя")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void ICanAddNewEmployee() throws SQLException {
+        Faker faker = new Faker();
+        Employee employee = generateEmployee(faker, companyId);
+
+        CreateEmployeeResponse response = employeeApiHelper.createEmployee(employee);
+
+        ResultSet resultSet = databaseService.getEmployeeInfo(response.id());
+        resultSet.next();
+        assertEquals(response.id(), resultSet.getInt(1));
+        assertEquals(employee.firstName(), resultSet.getString(2));
+        assertEquals(employee.lastName(), resultSet.getString(3));
+        assertEquals(employee.middleName(), resultSet.getString(4));
+       // assertEquals(employee.email(), resultSet.getString(6));
+        assertEquals(employee.avatar_url(), resultSet.getString(7));
+        assertEquals(employee.phone(), resultSet.getString(8));
+        assertEquals(employee.birthdate().toString(), resultSet.getDate(9).toString());
+        assertEquals(employee.isActive(), resultSet.getBoolean(10));
+
+    }
+
+    @Test
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void ICanEditEmployee() {
+
+        PatchEmployeeRequest patchEmployeeRequest = fakerEmployee();
+        Employee employee = employeeApiHelper.editEmployee(employeeId, patchEmployeeRequest);
+        assertEquals(employee.lastName(),patchEmployeeRequest.lastName());
+        assertEquals(employee.email(),patchEmployeeRequest.email());
+        assertEquals(employee.avatar_url(),patchEmployeeRequest.url());
+        assertEquals(employee.phone(),patchEmployeeRequest.phone());
+        assertEquals(employee.isActive(),patchEmployeeRequest.isActive());
+    }
+
+    @Test
+    @Description("Могу создать нового пользователя.Имя записалось корректно")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void ICanAddNewEmployeeFirstName() throws SQLException {
+        Faker faker = new Faker();
+        Employee employee = generateEmployee(faker, companyId);
+
+        CreateEmployeeResponse response = employeeApiHelper.createEmployee(employee);
+
+        ResultSet resultSet = databaseService.getEmployeeInfo(response.id());
+        resultSet.next();
+        assertEquals(response.id(), resultSet.getInt(1));
+        assertEquals(employee.firstName(), resultSet.getString(2));
+    }
+
+    @Test
+    @Description("Могу создать нового пользователя. Фамилия записалась корректно")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void ICanAddNewEmployeeLastName() throws SQLException {
+        Faker faker = new Faker();
+        Employee employee = generateEmployee(faker, companyId);
+
+        CreateEmployeeResponse response = employeeApiHelper.createEmployee(employee);
+
+        ResultSet resultSet = databaseService.getEmployeeInfo(response.id());
+        resultSet.next();
+        assertEquals(response.id(), resultSet.getInt(1));
+        assertEquals(employee.lastName(), resultSet.getString(3));
+    }
+
+    @Test
+    @Description("Могу создать нового пользователя. Отчество записалось корректно")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void ICanAddNewEmployeeMiddleName() throws SQLException {
+        Faker faker = new Faker();
+        Employee employee = generateEmployee(faker, companyId);
+
+        CreateEmployeeResponse response = employeeApiHelper.createEmployee(employee);
+
+        ResultSet resultSet = databaseService.getEmployeeInfo(response.id());
+        resultSet.next();
+        assertEquals(response.id(), resultSet.getInt(1));
+        assertEquals(employee.middleName(), resultSet.getString(4));
+    }
+
+    @Test
+    @Description("Могу создать нового пользователя. Email записан корректно")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void ICanAddNewEmployeeEmail() throws SQLException {
+        Faker faker = new Faker();
+        Employee employee = generateEmployee(faker, companyId);
+
+        CreateEmployeeResponse response = employeeApiHelper.createEmployee(employee);
+
+        ResultSet resultSet = databaseService.getEmployeeInfo(response.id());
+        resultSet.next();
+        assertEquals(response.id(), resultSet.getInt(1));
+        assertEquals(employee.email(), resultSet.getString(6), "EMAIL не записался");
+    }
+
+    @Test
+    @Description("Могу создать нового пользователя. Телефон записан корректно")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void ICanAddNewEmployeePhone() throws SQLException {
+        Faker faker = new Faker();
+        Employee employee = generateEmployee(faker, companyId);
+
+        CreateEmployeeResponse response = employeeApiHelper.createEmployee(employee);
+
+        ResultSet resultSet = databaseService.getEmployeeInfo(response.id());
+        resultSet.next();
+        assertEquals(response.id(), resultSet.getInt(1));
+        assertEquals(employee.phone(), resultSet.getString(8));
+
+    }
+
+    @Test
+    @Description("Могу создать нового пользователя. День рождения записан корректно")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void ICanAddNewEmployeeBirthdate() throws SQLException {
+        Faker faker = new Faker();
+        Employee employee = generateEmployee(faker, companyId);
+
+        CreateEmployeeResponse response = employeeApiHelper.createEmployee(employee);
+
+        ResultSet resultSet = databaseService.getEmployeeInfo(response.id());
+        resultSet.next();
+        assertEquals(response.id(), resultSet.getInt(1));
+        assertEquals(employee.birthdate(), resultSet.getDate(9).toString());
+    }
+
+    @Test
+    @Description("Могу создать нового пользователя. IsActive записан корректно")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void ICanAddNewEmployeeIsActive() throws SQLException {
+        Faker faker = new Faker();
+        Employee employee = generateEmployee(faker, companyId);
+
+        CreateEmployeeResponse response = employeeApiHelper.createEmployee(employee);
+
+        ResultSet resultSet = databaseService.getEmployeeInfo(response.id());
+        resultSet.next();
+        assertEquals(response.id(), resultSet.getInt(1));
+        assertEquals(employee.isActive(), resultSet.getBoolean(10));
+    }
+
+    @Test
+    @Description("Могу получить информацию о пользователе")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void ICanGetEmployeeInfo() {
+        Employee employee = employeeApiHelper.getEmployeeInfo(employeeId);
+        assertEquals(employee.id(), employeeId);
+        assertNotNull(employee.lastName());
+        assertNotNull(employee.firstName());
+    }
+
+    @Test
+    @Description("Могу получить список сотрудников по компании")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void ICanGetEmployeeListByCompany() {
+        List<Employee> employeeList = employeeApiHelper.getListOfEmployee(companyId);
+        assertTrue(employeeList.size() > 0);
+    }
+
+    public PatchEmployeeRequest fakerEmployee() {
+        Faker faker = new Faker();
+        return new PatchEmployeeRequest(faker.name().lastName(),
+                faker.internet().emailAddress(),
+                faker.internet().url(),
+                faker.phoneNumber().phoneNumber(),
+                faker.bool().bool());
+    }
+}
---
+++ src/test/java/api_inno_itog_project/EmployeeContractTest.java
@@ -0,0 +1,243 @@
+package api_inno_itog_project;
+
+import static api_inno_itog_project.constClass.ConstStatusCode.*;
+import static api_inno_itog_project.x_clients.helper.EmployeeRandomeService.generateEmployee;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+import com.github.javafaker.Faker;
+import api_inno_itog_project.x_clients.ext.DatabaseService;
+import api_inno_itog_project.x_clients.helper.CompanyApiHelper;
+import api_inno_itog_project.x_clients.helper.ConfProperties;
+import api_inno_itog_project.x_clients.helper.EmployeeApiHelper;
+import api_inno_itog_project.x_clients.model.AuthResponse;
+import api_inno_itog_project.x_clients.model.Employee;
+import api_inno_itog_project.x_clients.model.PatchEmployeeRequest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.sql.SQLException;
+import jdk.jfr.Description;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Epic("")
+public class EmployeeContractTest {
+
+    public static ConfProperties properties;
+    static DatabaseService databaseService;
+    static EmployeeApiHelper employeeApiHelper;
+    static CompanyApiHelper companyHelper;
+    static int companyId;
+    static int employeeId;
+    static String headersProperties;
+    private static String username;
+    private static String password;
+
+    @BeforeEach
+    public void setUp() throws SQLException {
+        properties = new ConfProperties();
+        RestAssured.baseURI = properties.getProperty("baseURI");
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        headersProperties = properties.getProperty("headers");
+        username = properties.getProperty("username");
+        password = properties.getProperty("password");
+        databaseService = new DatabaseService();
+        databaseService.connectToDb();
+        companyId = databaseService.createNewCompany();
+        employeeId = databaseService.createNewEmployee(companyId);
+        companyHelper = new CompanyApiHelper();
+        employeeApiHelper = new EmployeeApiHelper();
+    }
+
+
+    @AfterEach
+    public void tearDown() throws SQLException {
+        databaseService.deleteCompanyAndItsEmloyees(companyId);
+        databaseService.closeConnection();
+    }
+
+    @Test
+    @DisplayName("Получаем список работников по существующей компании")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void getListEmployeeByCompany() throws SQLException {
+        int id = databaseService.getAnyCompanyID();
+        given()
+            .basePath("employee")
+            .queryParam("company", id)
+            .when()
+            .get()
+            .then()
+            .statusCode(HTTP_CODE_OK)
+            .header("Content-Type", "application/json; charset=utf-8");
+    }
+
+    @Test
+    @Description("Ожидаем пустое тело по запросу списка работников по НЕ существующему id компании")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void getBodyEmployeeByCompany() throws SQLException {
+        int id = databaseService.getLastCompanyID();
+        given()
+            .basePath("employee")
+            .queryParam("company", id + 1)
+            .when()
+            .get()
+            .then()
+            .statusCode(HTTP_CODE_OK)
+            .header("Content-Type", "application/json; charset=utf-8")
+            .body(equalTo("[]"));
+    }
+
+    @Test
+    @Description("Ожидаем статус 200,при запросе сотрудника по его id")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void getEmployeeById() throws SQLException {
+        int id = databaseService.getAnyEmployeeId();
+        given()
+            .basePath("employee")
+            .when()
+            .get("{Id}", id)
+            .then()
+            .statusCode(HTTP_CODE_OK)
+            .header("Content-Type", "application/json; charset=utf-8")
+            .and()
+            .extract().response().equals(id);
+    }
+
+    @Test
+    @Description("Ожидаем статус 200 и Content-length=0, при получение сотрудника с несуществующем id")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void getEmployeerByInvalidId() throws SQLException {
+
+        int id = databaseService.getLastEmployeeID();
+        given()
+            .basePath("employee")
+            .when()
+            .get("{Id}", id + 1)
+            .then()
+            .statusCode(HTTP_CODE_OK)
+            .header("Content-length", Matchers.equalTo("0"))
+            .extract().response().equals(employeeId);
+    }
+
+    @Test
+    @Description("Проверяем, что не  можем создать сотрудника без токеном, status-401")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void iCannotAddNewEmployee() {
+        Faker faker = new Faker();
+        Employee createEmployeeRequest = generateEmployee(faker, companyId);
+        given()
+            .basePath("employee")
+            .body(createEmployeeRequest)
+            .contentType(ContentType.JSON)
+            .when()
+            .post()
+            .then()
+            .assertThat()
+            .statusCode(HTTP_CODE_UNAUTHORIZED);
+    }
+
+    @Test
+    @Description("Проверяем что можем создать сотрудника c токеном")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void iCanAddNewEmployee() {
+
+        AuthResponse info = employeeApiHelper.auth(username, password);
+        Faker faker = new Faker();
+        Employee createEmployeeRequest = generateEmployee(faker, companyId);
+        given()
+            .basePath("employee")
+            .body(createEmployeeRequest)
+            .header("x-client-token", info.userToken())
+            .contentType(ContentType.JSON)
+            .when()
+            .post()
+            .then()
+            .assertThat()
+            .statusCode(HTTP_CODE_CREATE);
+    }
+
+    @Test
+    @Description("Проверяем, что можем изменить информацию о сотруднике")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void iCanEditEmployee() {
+        AuthResponse info = employeeApiHelper.auth(username, password);
+        PatchEmployeeRequest patchEmployeeRequest = fakerEmployee();
+        given()
+            .basePath("employee")
+            .body(patchEmployeeRequest)
+            .header("x-client-token", info.userToken())
+            .contentType(ContentType.JSON)
+            .when()
+            .patch("{id}", employeeId)
+            .then()
+            .assertThat()
+            .statusCode(HTTP_CODE_OK);
+    }
+
+    @Test
+    @Description("Проверяем, что при отправке запросы на изменение несуществующего сотрудника получаем 500")
+    @DisplayName("")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void iCannotEditEmployee() {
+        AuthResponse info = employeeApiHelper.auth(username, password);
+        PatchEmployeeRequest patchEmployeeRequest = fakerEmployee();
+
+        given()
+            .basePath("employee")
+            .body(patchEmployeeRequest)
+            .header("x-client-token", info.userToken())
+            .contentType(ContentType.JSON)
+            .when()
+            .patch("{id}", employeeId + 1000)
+            .then()
+            .assertThat()
+            .statusCode(HTTP_CODE_INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    @DisplayName("Получение списка сотрудников для компании")
+    @Tag("")
+    @Story("")
+    @Severity(SeverityLevel.BLOCKER)
+    public void getEmployeeCompany() {
+        employeeApiHelper.printGetEmployeeIsCompany(companyId);
+    }
+
+    public PatchEmployeeRequest fakerEmployee() {
+        Faker faker = new Faker();
+        return new PatchEmployeeRequest(faker.name().lastName(),
+            faker.internet().emailAddress(),
+            faker.internet().url(),
+            faker.phoneNumber().phoneNumber(),
+            faker.bool().bool());
+    }
+}
