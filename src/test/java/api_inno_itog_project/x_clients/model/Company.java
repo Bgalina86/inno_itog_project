0cf7255a02bf7c340ec8aa0fb7a1fcf6a3f423ca
g.smirnova <g.smirnova@bftcom.com>
2024-12-02 08:04:47 +0300

    maven.yml

---
+++ src/test/java/api_inno_itog_project/x_clients/model/Company.java
@@ -0,0 +1,7 @@
+package api_inno_itog_project.x_clients.model;
+
+public class Company {
+            public void Company(int id, String name, String description, boolean isActive){
+        }
+    }
+
